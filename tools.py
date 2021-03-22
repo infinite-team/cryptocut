1f4f8824d448c587e4bc871dba714fcac032ab85
alitkp <workalitkp@gmail.com>
2021-03-23 03:30:14 +0430

    added symmetric encryotion & decryption plus some miner function name changes

--- tools.py
+++ tools.py
@@ -9,6 +9,8 @@
 
 import os
 import appdirs
+from cryptography.fernet import Fernet
+
 appname="CryptoCut"
 appauthor="Infinite"
 
@@ -21,12 +23,15 @@ log_path = appdirs.user_log_dir(appname,appauthor)
 dtate_path = appdirs.user_state_dir(appname,appauthor)
 
 
-def get_user_key(user_pass):
+def create_user_key(user_pass):
+
+    '''creating a key baseed on user key'''
     from cryptography.hazmat.primitives import hashes
     from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
     import base64
     from cryptography.hazmat.backends import default_backend
 
+    user_pass = user_pass.encode()
     # TODO : check if its the first time or user wants to change the pass
 
     # create a key based on user password
@@ -43,9 +48,23 @@ def get_user_key(user_pass):
     return user_key
 
 def read_symmetric_key(file_name = 'symmetric.key'):
+    # TODO : add a verification method for key 
     with open(file_name,'rb') as f:
         encrypted_key = f.read()
-    user_pass = input("enter your password : ").encode()
-    fernet = get_user_key(user_pass)
+    user_pass = input("enter your password : ")
+    userbase_key = create_user_key(user_pass)
+    fernet = Fernet(userbase_key)
     decrypted_key = fernet.decrypt(encrypted_key)
     return decrypted_key
+
+def symmetric__encrypt(message,key):
+    message = message.encode()
+    f = Fernet(key)
+    return(f.encrypt(message))
+
+def symmetric__decrypt(encrypted_message,key):
+    encrypted_message = encrypted_message.encode('utf-8')
+    f = Fernet(key)
+    return(f.decrypt(encrypted_message).decode('utf-8'))
+
+
