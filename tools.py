138e2d2fa28ba4015fd864cedc358e43fb55fa70
alitkp <workalitkp@gmail.com>
2021-03-23 02:57:41 +0430

    create and get symmetric key based on user password

---
+++ tools.py
@@ -0,0 +1,51 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2021, Infinite Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+
+
+import os
+import appdirs
+appname="CryptoCut"
+appauthor="Infinite"
+
+path = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(path, "Files", "words-list.txt")
+cache_path = appdirs.user_cache_dir(appname,appauthor)
+config_path = appdirs.user_config_dir(appname,appauthor)
+data_path = appdirs.user_data_dir(appname,appauthor)
+log_path = appdirs.user_log_dir(appname,appauthor)
+dtate_path = appdirs.user_state_dir(appname,appauthor)
+
+
+def get_user_key(user_pass):
+    from cryptography.hazmat.primitives import hashes
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    import base64
+    from cryptography.hazmat.backends import default_backend
+
+    # TODO : check if its the first time or user wants to change the pass
+
+    # create a key based on user password
+    # TODO : we can use os.urandom(16) or somthing that user want
+    salt = b'cryptocut_'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend())
+    user_key = base64.urlsafe_b64encode(kdf.derive(user_pass))
+
+    return user_key
+
+def read_symmetric_key(file_name = 'symmetric.key'):
+    with open(file_name,'rb') as f:
+        encrypted_key = f.read()
+    user_pass = input("enter your password : ").encode()
+    fernet = get_user_key(user_pass)
+    decrypted_key = fernet.decrypt(encrypted_key)
+    return decrypted_key
